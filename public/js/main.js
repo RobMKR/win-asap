9ab7831de79a8bb7d511a4449dc6505e973ab6a9
Rob <rob.mkrtchyan@mgplab.com>
2018-01-08 18:16:47 +0400

    Set Counter to 0

--- public/js/main.js
+++ public/js/main.js
@@ -4,41 +4,46 @@ $(document).ready(function() {
 
 		/* Homepage Countdown Script */
 
-		CountDown: function() {
+        CountDown: function() {
 
-			function countdown() {
+            function countdown() {
 
-				var now = new Date();
-				var eventDate = new Date(2017, 11, 30, 22, 0);
-				var currentTime = now.getTime();
-				var evenTime = eventDate.getTime();
+                var now = new Date();
+                var eventDate = new Date(2017, 11, 17, 22, 0);
+                var currentTime = now.getTime();
+                var evenTime = eventDate.getTime();
 
-				var remTime = evenTime - currentTime;
+                var remTime = evenTime - currentTime;
 
-				var sec = Math.floor(remTime / 1000);
-				var min = Math.floor(sec / 60);
-				var hur = Math.floor(min / 60);
-				var day = Math.floor(hur / 24);
+                var sec = Math.floor(remTime / 1000);
+                var min = Math.floor(sec / 60);
+                var hur = Math.floor(min / 60);
+                var day = Math.floor(hur / 24);
 
-				hur %= 24;
-				min %= 60;
-				sec %= 60;
+                hur %= 24;
+                min %= 60;
+                sec %= 60;
 
-				hur = (hur < 10) ? "0" + hur : hur;
-				min = (min < 10) ? "0" + min : min;
-				sec = (sec < 10) ? "0" + sec : sec;
+                // hur = (hur < 10) ? "0" + hur : hur;
+                // min = (min < 10) ? "0" + min : min;
+                // sec = (sec < 10) ? "0" + sec : sec;
 
-				$('.seconds').text(sec);
-				$('.minutes').text(min);
-				$('.hours').text(hur);
-				$('.days').text(day);
+                hur = 0;
+                min = 0;
+                sec = 0;
+                day = 0;
+
+                $('.seconds').text(sec);
+                $('.minutes').text(min);
+                $('.hours').text(hur);
+                $('.days').text(day);
 
 
-				setTimeout(countdown, 1000);
-			}
+                // setTimeout(countdown, 1000);
+            }
 
-			countdown();
-		}
+            countdown();
+        }
 
 	}
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -562,5 +562,5 @@
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="js/main.js?v=5"></script>
+    <script src="js/main.js?v=6"></script>
 @endsection
